6626d342a482e04e8ee464ec4c3a1d9fb13b4f5b
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-28 18:36:47 -0700

    are these changes really necessary? kind of excessive.

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def testAddition(self):
-        self.assertEqual(example.addition(3,5), 8)
+        self.assertEqual(example.addition(3, 5), 8)
 
 
 if __name__ == '__main__':
