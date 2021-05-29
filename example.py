616b77765bf843a8eb6f209a5a2496dd6a0a4319
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-28 18:27:54 -0700

    added code for example and unittesting for test.py

---
+++ example.py
@@ -0,0 +1,18 @@
+import math
+import random
+
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    return x - y
+
+def multiplication(x, y):
+    return x * y
+
+def division(x, y):
+    if x == 0 or y == 0:
+        return 0
+    
+    else:
+        return x / y
---
+++ test.py
@@ -0,0 +1,11 @@
+import exmaple
+import math
+import unittest
+
+class TestCase(unittest.TestCase):
+    def testAddition(self):
+        self.assertEqual(example.addition(3,5), 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
