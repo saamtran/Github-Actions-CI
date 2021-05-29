2c3d420de29503a685e460c42df7b320b032ee41
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-28 18:41:35 -0700

    fixed all errors!

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import example
 import unittest
 
-
+#adding white spaces is the stupidest thing ive ever seen. does not add to functionality.
 class TestCase(unittest.TestCase):
     def testAddition(self):
         self.assertEqual(example.addition(3, 5), 8)
